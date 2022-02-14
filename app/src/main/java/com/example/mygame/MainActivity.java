8a6ac8f82bd1f7f8d32f08a49d1fc722f760e8f4
Dmitry <dima.lippo@bk.ru>
2022-02-14 23:07:59 +0300

    5 Cards and fullscreen
    Added styles.xml
    Don't mind what happend with activity_main
    5 Card pictures added
    Fullscreen made in MainActivity.java

--- app/src/main/java/com/example/mygame/MainActivity.java
+++ app/src/main/java/com/example/mygame/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mygame;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.internal.NavigationMenu;
+import com.google.android.material.navigation.NavigationBarMenu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,    //Hiding status bar
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();   //Hiding title bar
+
+        View decorView = getWindow().getDecorView();    //Hiding navigation bar
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+
     }
 }
 
